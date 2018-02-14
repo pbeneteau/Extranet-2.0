77cae46faece87e55434286bdc2239f7afc0bdfc
Paul Bénéteau <paulbeneteau@pauls-macbook-pro.local>
2018-02-14 17:42:41 +0100

    Initial Commit

---
+++ Extranet 2.0/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Extranet 2.0
+//
+//  Created by Paul Bénéteau on 14/02/2018.
+//  Copyright © 2018 Paul Bénéteau. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
